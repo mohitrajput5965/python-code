24646ffecb542bfb6352986d12b9a1ff04649d3d
Mohit kumar singh <149250921+mohitrajput5965@users.noreply.github.com>
2023-12-15 11:54:17 +0530

    Add files via upload

---
+++ breakcontinuepass.py
@@ -0,0 +1,15 @@
+s="python"
+for i in s:
+    if i=="t":
+        break
+    print(i)
+
+for i in s:
+    if i=="t":
+        continue
+    print(i)
+
+for i in s:
+    if i=="t":
+        pass
+    print(i)
